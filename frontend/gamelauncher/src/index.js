0fce12679ec94f7af9b1568f7e0e76197f121011
Chadrak Holondo <89146273+chadrakh@users.noreply.github.com>
2023-02-09 17:50:54 +0000

    added nav persistence

--- frontend/gamelauncher/src/index.js
+++ frontend/gamelauncher/src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './index.css';
 import ReactDOM from 'react-dom/client';
-
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter,
+  Routes,
+  Route
 } from "react-router-dom";
-import { routes } from './routes/routes';
-
-const router = createBrowserRouter(routes);
+import { pageRoutes } from './routes/pageRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route key={pageRoutes[0].id} path={pageRoutes[0].path} element={pageRoutes[0].element}>
+          {pageRoutes.map((page) => (
+            <Route key={page.id} path={page.path} element={page.element} />
+          ))}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, learn more here: https://bit.ly/CRA-vitals
+// If you want to start measuring performance in your app, learn more here:
+// https://bit.ly/CRA-vitals
--- frontend/gamelauncher/src/routes/pageRoutes.js
+++ frontend/gamelauncher/src/routes/pageRoutes.js
@@ -5,31 +5,37 @@ import Support from "../pages/Support/Support";
 import Register from "../pages/Register/Register";
 import Login from "../pages/Login/Login";
 
-const routes = [
+const pageRoutes = [
     {
+        id: 0,
         path: "/",
         element: <App />,
     },
     {
+        id: 1,
         path: "/store",
         element: <Store />,
     },
     {
+        id: 2,
         path: "/community",
         element: <Community />,
     },
     {
+        id: 3,
         path: "/support",
         element: <Support />,
     },
     {
+        id: 4,
         path: "/register",
         element: <Register />,
     },
     {
+        id: 5,
         path: "/login",
         element: <Login />,
     },
 ]
 
-export { routes };
+export { pageRoutes };
